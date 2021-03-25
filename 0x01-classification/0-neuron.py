e83255af90e6e0ea1b64d5c4a540c3cb57806c75
oumaima-mecheheb <78688037+oumaima-mecheheb@users.noreply.github.com>
2021-03-25 21:10:28 +0000

    Update 0-neuron.py

--- 0x01-classification/0-neuron.py
+++ 0x01-classification/0-neuron.py
@@ -2,17 +2,15 @@
 
 import numpy as np
 
-Neuron = __import__('0-neuron').Neuron
 
-lib_train = np.load('../data/Binary_Train.npz')
-X_3D, Y = lib_train['X'], lib_train['Y']
-X = X_3D.reshape((X_3D.shape[0], -1)).T
-
-np.random.seed(0)
-neuron = Neuron(X.shape[0])
-print(neuron.W)
-print(neuron.W.shape)
-print(neuron.b)
-print(neuron.A)
-neuron.A = 10
-print(neuron.A)
+class Neuron:
+   
+    def __init__(self, nx):
+        
+        if not isinstance(nx, int):
+            raise TypeError('nx must be an integer')
+        if nx < 1:
+            raise ValueError('nx must be a positive integer')
+        self.W = np.random.normal(0, 1, (1, nx))
+        self.b = 0
+        self.A = 0
