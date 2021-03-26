136e06c700fd78989b6144a341ee9a61d6dfadb9
oumaima-mecheheb <78688037+oumaima-mecheheb@users.noreply.github.com>
2021-03-26 18:40:59 +0000

    Update 5-neuron.py

--- 0x01-classification/5-neuron.py
+++ 0x01-classification/5-neuron.py
@@ -83,16 +83,3 @@ class Neuron():
         db = np.sum(dz) / m  # grad of the loss with respect to b
         self.__W -= (alpha * dW).T
         self.__b -= alpha * db
-
-Neuron = __import__('4-neuron').Neuron
-
-lib_train = np.load('C:/Users/lenovo/Desktop/OumaimaI/ml/0x01-classification/data/Binary_Train.npz')
-X_3D, Y = lib_train['X'], lib_train['Y']
-X = X_3D.reshape((X_3D.shape[0], -1)).T
-
-np.random.seed(0)
-neuron = Neuron(X.shape[0])
-A = neuron.forward_prop(X)
-neuron.gradient_descent(X, Y, A, 0.5)
-print(neuron.W)
-print(neuron.b)
