eebea97ee40ba6a0d00d2a86425dc030849bbea2
oumaima-mecheheb <78688037+oumaima-mecheheb@users.noreply.github.com>
2021-03-26 17:40:09 +0000

    Update 0-neuron.py

--- 0x01-classification/0-neuron.py
+++ 0x01-classification/0-neuron.py
@@ -4,13 +4,13 @@ import numpy as np
 
 
 class Neuron:
-   
+   """ Class Neuron """
    
     def __init__(self, nx):
         if not isinstance(nx, int):
             raise TypeError('nx must be an integer')
         if nx < 1:
             raise ValueError('nx must be a positive integer')
-        self.W = np.random.normal(0, 1, (1, nx))
-        self.b = 0
-        self.A = 0
+        self.W = np.random.normal(0, 1, (1, nx)) # Weight
+        self.b = 0 # Bias
+        self.A = 0 # Output
