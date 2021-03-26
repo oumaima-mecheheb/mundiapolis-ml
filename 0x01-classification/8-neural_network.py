8748835bb95415db56245f74f5a7eb270f2f2b92
oumaima-mecheheb <78688037+oumaima-mecheheb@users.noreply.github.com>
2021-03-26 22:15:30 +0000

    Update 8-neural_network.py

--- 0x01-classification/8-neural_network.py
+++ 0x01-classification/8-neural_network.py
@@ -2,9 +2,11 @@
 
 import numpy as np
 
+
 class NeuralNetwork():
     """ Class neural network"""
 
+    
     def __init__(self, nx, nodes):
 
         if type(nx) is not int:
@@ -22,22 +24,3 @@ class NeuralNetwork():
         self.W2 = np.random.randn(nodes).reshape(1, nodes)
         self.b2 = 0
         self.A2 = 0
-
-NN = __import__('8-neural_network').NeuralNetwork
-
-lib_train = np.load('C:/Users/lenovo/Desktop/OumaimaI/ml/0x01-classification/data/Binary_Train.npz')
-X_3D, Y = lib_train['X'], lib_train['Y']
-X = X_3D.reshape((X_3D.shape[0], -1)).T
-
-np.random.seed(0)
-nn = NN(X.shape[0], 3)
-print(nn.W1)
-print(nn.W1.shape)
-print(nn.b1)
-print(nn.W2)
-print(nn.W2.shape)
-print(nn.b2)
-print(nn.A1)
-print(nn.A2)
-nn.A1 = 10
-print(nn.A1)
