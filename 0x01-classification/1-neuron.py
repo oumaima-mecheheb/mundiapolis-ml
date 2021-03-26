f965529b94d52d45c46629a8bd555767b925e218
oumaima-mecheheb <78688037+oumaima-mecheheb@users.noreply.github.com>
2021-03-26 17:37:49 +0000

    Update 1-neuron.py

--- 0x01-classification/1-neuron.py
+++ 0x01-classification/1-neuron.py
@@ -39,18 +39,3 @@ class Neuron():
 
         """
         return self.__A
-
-Neuron = __import__('1-neuron').Neuron
-
-lib_train = np.load('C:/Users/lenovo/Desktop/OumaimaI/ml/0x01-classification/data/Binary_Train.npz')
-X_3D, Y = lib_train['X'], lib_train['Y']
-X = X_3D.reshape((X_3D.shape[0], -1)).T
-
-
-np.random.seed(0)
-neuron = Neuron(X.shape[0])
-print(neuron.W)
-print(neuron.b)
-print(neuron.A)
-neuron.A = 10
-print(neuron.A)
