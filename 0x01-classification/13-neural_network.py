cf10ba6377bdb62b31507d194d13f7e7fbefc741
oumaima-mecheheb <78688037+oumaima-mecheheb@users.noreply.github.com>
2021-04-02 16:56:33 +0100

    Update 13-neural_network.py

--- 0x01-classification/13-neural_network.py
+++ 0x01-classification/13-neural_network.py
@@ -2,6 +2,7 @@
 
 import numpy as np
 
+
 class NeuralNetwork():
     """ Class neural network"""
 
@@ -124,18 +125,3 @@ class NeuralNetwork():
         self.__b2 = self.__b2 - alpha * db2
         self.__W1 = self.__W1 - alpha * dw1.T
         self.__b1 = self.__b1 - alpha * db1
-
-NN = __import__('13-neural_network').NeuralNetwork
-
-lib_train = np.load('C:/Users/lenovo/Desktop/OumaimaI/ml/0x01-classification/data/Binary_Train.npz')
-X_3D, Y = lib_train['X'], lib_train['Y']
-X = X_3D.reshape((X_3D.shape[0], -1)).T
-
-np.random.seed(0)
-nn = NN(X.shape[0], 3)
-A1, A2 = nn.forward_prop(X)
-nn.gradient_descent(X, Y, A1, A2, 0.5)
-print(nn.W1)
-print(nn.b1)
-print(nn.W2)
-print(nn.b2)
